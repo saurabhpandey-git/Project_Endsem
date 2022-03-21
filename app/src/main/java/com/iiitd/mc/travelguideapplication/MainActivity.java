eddedc91135998f61bee27fbdd9b933655c34fe3
Yukti Goswami <yuktig.jaipur@gmail.com>
2022-03-21 16:50:03 +0530

    UI design complete Home screen

--- app/src/main/java/com/iiitd/mc/travelguideapplication/MainActivity.java
+++ app/src/main/java/com/iiitd/mc/travelguideapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.iiitd.mc.travelguideapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragMan = getSupportFragmentManager();
+        Fragment F = fragMan.findFragmentById(R.id.fragmentHomeScreen);
+        if(F == null){
+            Log.i("Fragment", "Fragment is null");
+            F = new HomeScreenFragment();
+            fragMan.beginTransaction().add(R.id.fragmentHomeScreen,F).commit();
+        }
+        Log.i("Fragment", "After inflating");
+
+
     }
 }
